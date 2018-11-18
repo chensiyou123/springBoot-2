e6f2977537e9f79209a416b1312e9e63ac3e3934
chensiyou <905630790@qq.com.cn>
2018-11-18 19:45:52 +0800

    springBoot整合jquery、json、angular

--- project/src/main/java/com/csy/springbootweb/controller/HomeController.java
+++ project/src/main/java/com/csy/springbootweb/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.csy.springbootweb.controller;
 
+import com.csy.springbootweb.entity.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -20,5 +26,41 @@ public class HomeController {
         map.put("age", "12");
         return "hello";
 
+    }
+
+
+    @RequestMapping("/jquery")
+    public String jquery() {
+        return "jquery";
+    }
+
+    @RequestMapping(value ="/angularjs",method = RequestMethod.GET)
+    public String angularjs() {
+        return "angularjs";
+    }
+    @ResponseBody
+    @RequestMapping(value = "/postData",method = RequestMethod.POST)
+    public  Map<String, Object> postData(String no, int quantity, String date) {
+        System.out.println("no:" + no);
+        System.out.println("quantity:" + quantity);
+        System.out.println("date:" + date);
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "ok");
+        map.put("quantity", quantity);
+        map.put("no", no);
+        map.put("date", date);
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/postJson",method = RequestMethod.POST)
+    public  Map<String, Object> postJson(@RequestBody Order order) {
+        System.out.println("order no:" + order.no);
+        System.out.println("order quantity:" + order.quantity);
+        System.out.println("order date:" + order.date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "ok");
+        map.put("value", order);
+        return map;
     }
 }
---
+++ project/src/main/java/com/csy/springbootweb/entity/Order.java
@@ -0,0 +1,34 @@
+package com.csy.springbootweb.entity;
+
+import java.util.Date;
+
+public class Order {
+
+    public String no;
+    public Date date;
+    public int quantity;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
