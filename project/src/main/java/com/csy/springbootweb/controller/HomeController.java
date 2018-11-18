e43679b1a6fd79e4e7a11c4ef862e7f47f2ff3b9
chensiyou <905630790@qq.com.cn>
2018-11-18 19:24:42 +0800

    thymeleaf

--- project/src/main/java/com/csy/springbootweb/controller/HomeController.java
+++ project/src/main/java/com/csy/springbootweb/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.csy.springbootweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
+@Controller
 public class HomeController {
     @RequestMapping("/")
-    public @ResponseBody
-    String index() {
-        return "你好，这是第一个spring boot应用";
+    public String index(Model model) {
+        model.addAttribute("name", "111");
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Map<String, Object> map) {
+        map.put("name", "fanfan");
+        map.put("age", "12");
+        return "hello";
+
     }
 }
